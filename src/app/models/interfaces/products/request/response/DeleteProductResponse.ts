563cdd14d594a120171fb634913189876f340e5a
Eduardo Andrade Ferreira <135661746+eduardoanfer@users.noreply.github.com>
2025-05-16 14:34:10 -0300

    fix/01

--- src/app/models/interfaces/products/request/response/DeleteProductResponse.ts
+++
@@ -1,8 +0,0 @@
-export interface DeleteProductResponse {
-id: string; // id do produto
-name: string; // nome do produto
-price: string; // preco do produto
-amount: number; // quantidade do produto
-description: string; // descricao do produto
-category_id: string; // id da categoria
-}
